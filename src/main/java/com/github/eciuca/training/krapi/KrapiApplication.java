bcb1e229ecdda017d3478d4b47be98f36d30bd6a
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2023-11-22 01:43:12 +0200

    Configure CORS

--- src/main/java/com/github/eciuca/training/krapi/KrapiApplication.java
+++ src/main/java/com/github/eciuca/training/krapi/KrapiApplication.java
@@ -2,6 +2,11 @@ package com.github.eciuca.training.krapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class KrapiApplication {
@@ -10,4 +15,20 @@ public class KrapiApplication {
 		SpringApplication.run(KrapiApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
+	}
+
+	@Bean
+	public RepositoryRestConfigurer repositoryRestConfigurer() {
+		return RepositoryRestConfigurer.withConfig((config, corsRegistry) -> {
+			corsRegistry.addMapping("/**");
+		});
+	}
 }
