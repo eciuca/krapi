2a5233de58aab68223a4fd752c32e075fe17afc9
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2023-11-23 10:12:18 +0200

    Generate ids automatically for Student

--- src/main/java/com/github/eciuca/training/krapi/domain/Student.java
+++ src/main/java/com/github/eciuca/training/krapi/domain/Student.java
@@ -14,6 +14,7 @@ import lombok.ToString;
 @ToString(onlyExplicitlyIncluded = true)
 public class Student {
     @Id
+    @GeneratedValue
     @EqualsAndHashCode.Include
     private Long id;
     @Column(name = "CLASS")
